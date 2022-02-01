f3486d56bdf43ef01f94fb8558464edf21c86069
Gestalte <philipbenade@gmail.com>
2022-02-01 17:31:34 +0200

    Added removing dashes and title case.

--- pdf-renamer/Program.fs
+++ pdf-renamer/Program.fs
@@ -1,4 +1,6 @@
 ﻿open System.IO
+open System
+open System.Globalization
 
 let currentDirectory = Directory.GetCurrentDirectory()
 
@@ -9,7 +11,10 @@ let RenameFile (path:string, newName:string) =
         Path.GetExtension(path)
     let newPath = $"{dir()}\{newName}{extension()}"
 
-    if path = newPath then
+    System.Diagnostics.Debug.WriteLine(path)
+    System.Diagnostics.Debug.WriteLine(newPath)
+
+    if (path = newPath) then
         ()
     else
         Directory.Move(path, newPath)
@@ -17,20 +22,32 @@ let RenameFile (path:string, newName:string) =
         System.Console.WriteLine(Path.GetFileNameWithoutExtension(newPath))
 
 let GetNewName (path:string) =
+    
     let removeZlib (name:string) =
-        name.Replace("(z-lib.org)","")
+        name.Replace("(z-lib.org)","").Replace("(Z Lib.Org)","")
 
     let removeUnderscore (name:string) =
         name.Replace("_"," ")
 
+    let removeDash (name:string) =
+        name.Replace("-"," ")
+
     let TrimString (name:string) =
         name.Trim()
 
-    Path.GetFileNameWithoutExtension(path)
-    |> removeUnderscore
-    |> removeZlib
-    |> TrimString
+    let PrintState(name:string) = 
+        System.Diagnostics.Debug.WriteLine(name)
+        name
 
+    Path.GetFileNameWithoutExtension(path)
+    |> PrintState
+    |> System.Globalization.CultureInfo.InvariantCulture.TextInfo.ToTitleCase
+    |> PrintState
+    |> removeZlib
+    |> removeUnderscore
+    |> removeDash
+    |> TrimString
+    
 let renameFiles (path:string) =
     Directory.GetFiles path 
     |> Array.map (fun a -> (a, GetNewName(a)))
